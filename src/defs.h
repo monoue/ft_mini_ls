55a934edf46914dc351d2fde65c01d16978fb4a0
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2020-12-03 16:45:43 +0900

    nsec becomes 0 problem

--- src/defs.h
+++ src/defs.h
@@ -6,12 +6,14 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 13:34:12 by monoue            #+#    #+#             */
-/*   Updated: 2020/12/03 11:43:48 by monoue           ###   ########.fr       */
+/*   Updated: 2020/12/03 15:15:07 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFS_H
 # define DEFS_H
+
+# include "debug.h"
 
 # define CURRENT_DIR_PATH	"."
 # define ARG_ERR "Arg not permitted\n"
--- src/main.c
+++ src/main.c
@@ -6,30 +6,95 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 13:33:20 by monoue            #+#    #+#             */
-/*   Updated: 2020/12/03 13:34:59 by monoue           ###   ########.fr       */
+/*   Updated: 2020/12/03 16:42:21 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mini_ls.h"
 
-
-
-size_t	count_files(void)
+int	count_files(void)
 {
 	DIR				*dir;
 	size_t			files_num;
+	struct dirent	*dir_ent;
 
 	dir = opendir(CURRENT_DIR_PATH);
 	if (dir == NULL)
-	{
-		perror("opendir");
-		return (0);
-	}
+		return (ERROR);
 	files_num = 0;
-	while (readdir(dir))
+	while ((dir_ent = readdir(dir)) != NULL)
+	{
+		if (dir_ent->d_name[0] == '.')
+			continue ;
 		files_num++;
+	}
 	closedir(dir);
-	return (files_num - 1);
+	return (files_num);
+}
+
+// void	put_file_names(t_stat *dent_stats_arr, int files_num)
+// {
+// 	int	index;	
+	
+// 	index = 0;
+// 	while (index < files_num)
+// 	{
+// 		// ft_putendl();
+// 		index++;
+// 	}
+// }
+
+typedef struct	s_file_data {
+	char	*name;
+	time_t	tv_sec;
+
+	long	tv_nsec;	
+	// long	tv_nsec;	
+}				t_file_data;
+#include <stdio.h>
+
+
+void	put_file_data_arr(t_file_data *file_data_arr, int files_num)
+{
+	int	index;
+
+	t_file_data file_data;
+
+	index = 0;
+	while (index < files_num)
+	{
+		file_data = file_data_arr[index];
+		// printf("name: %s, sec: %ld\n", file_data.name, file_data.tv_sec);
+		// DS(file_data.name);
+		// DL(file_data.tv_sec);
+		// DL(file_data.tv_nsec);
+		index++;
+	}
+}
+
+void	sort_file_data_arr(t_file_data *file_data_arr, int files_num)
+{
+	
+	int left;
+	int	right;
+	t_file_data tmp;
+
+	left = 0;
+	while (left + 1 < files_num)
+	{
+		right = left + 1;
+		while (right < files_num)
+		{
+			if (file_data_arr[left].tv_sec > file_data_arr[right].tv_sec)
+			{
+				tmp = file_data_arr[left];
+				file_data_arr[left] = file_data_arr[right];
+				file_data_arr[right] = tmp;
+			}
+			right++;
+		}
+		left++;
+	}
 }
 
 static void	list_dir(void)
@@ -46,17 +111,44 @@ static void	list_dir(void)
 	// dent_stat の配列用意
 	// 開き直して、数の分 malloc ?
 	
+	// t_stat	*dent_stats_arr;
+	t_file_data *file_data_arr;
+
+	int files_num = count_files();
+	if (files_num == ERROR)
+	{
+		perror("opendir");
+		return ;
+	}
+	// dent_stats_arr = malloc(sizeof(t_stat) * (files_num + 1));
+	file_data_arr = malloc(sizeof(t_file_data) * (files_num + 1));
+
+	
+	size_t	index;
+	index = 0;
 	while ((dir_ent = readdir(dir)) != NULL)
 	{
 		t_stat	dent_stat;
 		char *d_name = dir_ent->d_name;
 		if (d_name[0] == '.')
 			continue ;
+		// if (lstat(d_name, &dent_stats_arr[index]) != SUCCESS)
 		if (lstat(d_name, &dent_stat) != SUCCESS)
 		{
 			perror(d_name);
 			continue ;
 		}
+		file_data_arr[index].name = d_name;
+		printf("%ld\n", dent_stat.st_mtimespec.tv_sec);
+		printf("%ld\n", dent_stat.st_mtimespec.tv_nsec);
+		file_data_arr[index].tv_sec = dent_stat.st_mtimespec.tv_sec;
+		// DL(dent_stat.st_mtimespec.tv_nsec);
+
+		file_data_arr[index].tv_nsec = dent_stat.st_mtimespec.tv_nsec;
+
+		// file_data_arr[index].name = d_name;
+		// file_data_arr[index].tv_sec = dent_stat.st_mtimespec.tv_sec;
+		
 		// dent_stat の配列に入れていく
 		// put_l_option(dent_stat);
 		// bool color = true;
@@ -65,12 +157,16 @@ static void	list_dir(void)
 		// if (color == true)
 		// 	put_name_with_option_gg(d_name, dent_stat.st_mode, link_ok);
 		// else
-			ft_putendl(d_name);
+			// ft_putendl(d_name);
 		// put_ff_option(dent_stat.st_mode)	;
+		index++;
 	}
+	closedir(dir);
+	sort_file_data_arr(file_data_arr, files_num);
+	// sort_dent_stats(dent_stats_arr);
+	put_file_data_arr(file_data_arr, files_num);
 	// 配列をソート
 
-	closedir(dir);
 }
 
 int	main(int argc, char *argv[])
